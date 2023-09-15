a4050ce1d602ff9d29bf867cb4a28d8773d460ad
Zhaoyang-Jia <z4jia@ucsd.edu>
2023-09-15 16:08:34 -0700

    modified Extract_Genome.py so that it accepts an identical mapping for chr_name_file

--- Preparation/Extract_Genome.py
+++ Preparation/Extract_Genome.py
@@ -4,17 +4,24 @@ import IO
 def Extract_Genome(chr_of_interest: [str], genome_path: str, chr_name_file: str, output_file: str):
     chr_name_conversion = {}
     full_name_list = []
-    with open(chr_name_file) as fp_read:
-        for line in fp_read:
-            line = line.replace("\n", "").split("\t")
-            if line[0] in chr_of_interest:
-                chr_name_conversion[line[1]] = line[0]
-                full_name_list.append(line[1])
+    if chr_name_file == "none":
+        full_name_list = chr_of_interest
+    else:
+        with open(chr_name_file) as fp_read:
+            for line in fp_read:
+                line = line.replace("\n", "").split("\t")
+                if line[0] in chr_of_interest:
+                    chr_name_conversion[line[1]] = line[0]
+                    full_name_list.append(line[1])
+
     sequence_dict = IO.read_FASTA(genome_path, full_name_list)
 
     new_sequence_dict = {}
     for header, sequence in sequence_dict.items():
-        new_header = chr_name_conversion[header]
+        if chr_name_file == "none":
+            new_header = header
+        else:
+            new_header = chr_name_conversion[header]
         new_sequence_dict[new_header] = sequence
 
     IO.sequence_dict_to_FASTA(new_sequence_dict, output_file)
@@ -30,6 +37,12 @@ def Extract_Genome(chr_of_interest: [str], genome_path: str, chr_name_file: str,
 #                '../Genomes/GCF_000001405.26_GRCh38_genomic.fasta', '../Metadata/Chr_Names.txt',
 #                'Chr14Chr21ChrXY.fasta')
 
-Extract_Genome(['Chr12', 'Chr21', 'ChrX', 'ChrY'],
-               '../Genomes/GCF_000001405.26_GRCh38_genomic.fasta', '../Metadata/Chr_Names.txt',
-               'Chr12Chr21ChrXY.fasta')
+
+def one_time_usage():
+    Extract_Genome(['ChrXa'],
+                   '/media/zhaoyang-new/workspace/KarSim/0910_test/FASTA/23Xe10_r1.fasta', "none",
+                   '/media/zhaoyang-new/workspace/KarSim/0915_test/23Xe10_r1_ChrX.fasta')
+
+
+if __name__ == "__main__":
+    one_time_usage()
